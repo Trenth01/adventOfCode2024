9f000bdd7f81a7af7cc968e326fd247bc3bfa606
Trenth01 <96491309+Trenth01@users.noreply.github.com>
2024-12-01 14:39:23 +0000

    day 1

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <functional>
+#include <map>
+
+#include "solutions/day1.hpp"
+//
+// Created by Trent on 12/1/2024.
+//
+
+
+vector<string> fileInput(const string& fileName) {
+    const string filePath = "../inputs/" + fileName;
+    ifstream inputFile = ifstream(filePath);
+    vector<string> lines;
+    string line;
+
+    if (!inputFile) {
+        cerr << "Unable to open file" << endl;
+        return lines;
+    }
+
+    while (getline(inputFile, line)) {
+        lines.push_back(line);
+    }
+
+    inputFile.close();
+    return lines;
+}
+
+void solveDay(int day, const function<tuple<int, int>(const vector<string>&)>& solveFunction) {
+    const string inputFile = "day" + to_string(day) + ".txt";
+    const vector<string> inputText = fileInput(inputFile);
+
+    auto [part1, part2] = solveFunction(inputText);
+    cout << "Day " << day << " solutions: \n"
+         << "Part 1: " << part1 << "\n"
+         << "Part 2: " << part2 << endl;
+}
+
+int main() {
+
+    map<int, function<tuple<int, int>(const vector<string>&)>> solutions = {
+        {1, day1}
+        // Add more days here, e.g., {2, day2}, {3, day3}, etc.
+    };
+    int day = 1;
+    if (solutions.contains(day)) {
+        solveDay(day, solutions[day]);
+    } else {
+        cerr << "Solution for day " << day << " not implemented!" << endl;
+    }
+
+    return 0;
+}
+
---
+++ solutions/day1.hpp
@@ -0,0 +1,52 @@
+#pragma once
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+inline int part1(const vector<int>& leftList, const vector<int>& rightList) {
+    vector<int> sortedLeft = leftList;
+    vector<int> sortedRight = rightList;
+    ranges::sort(sortedLeft);
+    ranges::sort(sortedRight);
+
+    int totalDiff = 0;
+    for (int i = 0; i < sortedLeft.size(); i++) {
+        totalDiff += abs(sortedLeft[i] - sortedRight[i]);
+    }
+
+    return totalDiff;
+
+}
+
+inline int part2(const vector<int>& leftList, vector<int> rightList) {
+    int totalDiff = 0;
+    unordered_map<int, int> rightMap;
+
+    for (int num : rightList) {
+        rightMap[num]++;
+    }
+    for (int i : leftList) {
+        if (rightMap.contains(i)) {
+            totalDiff += i * rightMap[i];
+        }
+    }
+    return totalDiff;
+}
+
+
+inline tuple<int, int> day1(const vector<string>& input) {
+    vector<int> leftList;
+    vector<int> rightList;
+    for (const auto & i : input) {
+        istringstream ss(i);
+        int left, right;
+        ss >> left >> right;
+        leftList.push_back(left);
+        rightList.push_back(right);
+    }
+    return {part1(leftList, rightList), part2(leftList, rightList)};
+}
